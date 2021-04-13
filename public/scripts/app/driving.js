f10d8f68cb42627af12cfccf183ee1d599bae1f4
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-04-13 22:26:04 +0200

    retrieve user's routes - close #37

--- public/scripts/app/driving.js
+++ public/scripts/app/driving.js
@@ -1,3 +1,35 @@
-window.app.driving = function initDriving() {
+import 'regenerator-runtime'
+import Cookie from '../utils/Cookie'
 
+window.app.driving = async function initDriving () {
+  const journey = new Journey()
+  await journey.fetchRoutes()
+  journey.debug()
+}
+
+class Journey {
+  constructor () {
+    this.strapiURL = 'https://carstats-backend.herokuapp.com'
+    this.routes = []
+  }
+
+  async fetchRoutes () {
+    const cookies = new Cookie()
+    const jwt = cookies.get('jwt')
+
+    const response = await fetch(`${this.strapiURL}/travels/me`, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwt}`
+      }),
+      redirect: 'follow'
+    })
+
+    this.routes = await response.json()
+  }
+
+  debug () {
+    console.log(this.routes)
+  }
 }
