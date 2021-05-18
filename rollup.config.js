93133d6c181f13156de04335a5f94c597cc26661
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-18 22:25:41 +0200

    clean rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -10,22 +10,6 @@ import nodePolyfills from 'rollup-plugin-node-polyfills'
 import externalGlobals from 'rollup-plugin-external-globals'
 
 export default [
-  // {
-  //   input: 'node_modules/mapbox-gl/dist/mapbox-gl.js',
-  //   output: {
-  //     file: 'dist/mapbox-gl.esm.min.js',
-  //     format: 'es'
-  //   },
-  //   plugins: [cjs()],
-  // },
-  // {
-  //   input: 'node_modules/@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.js',
-  //   output: {
-  //     file: 'dist/mapbox-gl-directions.esm.min.js',
-  //     format: 'es',
-  //   },
-  //   plugins: [terser(), cjs()],
-  // },
   ...[
     {
       input: 'public/scripts/auth/index.js',
@@ -46,14 +30,11 @@ export default [
       plugins: [
         nodePolyfills(),
         resolve(),
-        // babel({ babelHelpers: 'bundled' }),
+        babel({ babelHelpers: 'bundled' }),
         image(),
         json(),
         cjs(),
-        // externalGlobals({
-        //   '!mapbox-gl': 'mapboxgl',
-        //   'mapbox-gl-directions': 'MapboxDirections'
-        // })
+        terser()
       ]
     }
   }),
