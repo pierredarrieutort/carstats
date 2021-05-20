2952cb38c916f0e28b7d5c93b9be1a99e38df3b9
Mathieu Daix <mathieudaixpro@gmail.com>
2021-05-20 20:35:12 +0200

    add style to statistics page - close #88

--- public/scripts/app/statistics.js
+++ public/scripts/app/statistics.js
@@ -2,36 +2,40 @@ import dayjs from 'dayjs/esm/index.js'
 import * as dayjsDuration from 'dayjs/plugin/duration.js'
 import * as dayjsRelativeTime from 'dayjs/plugin/relativeTime.js'
 
+import leaderboard1 from '../../images/leaderboard-1.png'
+import leaderboard2 from '../../images/leaderboard-2.png'
+import leaderboard3 from '../../images/leaderboard-3.png'
+
 import { StatsApi } from '../utils/Api.js'
 
-export default function initStatistics () {
+export default function initStatistics() {
   const stats = new Stats()
   stats.init()
 }
 
 class Stats {
-  constructor () {
+  constructor() {
     this.statsApi = new StatsApi()
     this.leaderboards = []
     this.domLeaderboard = document.getElementById('leaderboards')
   }
 
-  async init () {
+  async init() {
     await this.fetchLeaderboard()
     this.displayLeaderboards()
   }
 
-  async fetchLeaderboard () {
+  async fetchLeaderboard() {
     this.leaderboards = await this.statsApi.leaderboard()
   }
 
-  displayLeaderboards () {
-    this.prepareLeaderboard(this.leaderboards.maxSpeedSanitized, 'vMax', 'By speed')
-    this.prepareLeaderboard(this.leaderboards.maxDistanceSanitized, 'totalDistance', 'By distance')
-    this.prepareLeaderboard(this.leaderboards.totalDurationSanitized, 'totalTravelDuration', 'By duration')
+  displayLeaderboards() {
+    this.prepareLeaderboard(this.leaderboards.maxSpeedSanitized, 'vMax', 'Ranking by speed')
+    this.prepareLeaderboard(this.leaderboards.maxDistanceSanitized, 'totalDistance', 'Ranking by distance')
+    this.prepareLeaderboard(this.leaderboards.totalDurationSanitized, 'totalTravelDuration', 'Ranking by duration')
   }
 
-  prepareLeaderboard (data, property, title) {
+  prepareLeaderboard(data, property, title) {
     const leaderboardItem = document.createElement('div')
     const leaderboardTitle = document.createElement('h2')
 
@@ -39,9 +43,27 @@ class Stats {
 
     leaderboardItem.append(leaderboardTitle)
 
+    let positionNumber = 1
+
     data.forEach(leaderboard => {
+      const leaderboardElement = document.createElement('div')
+      const leaderboardIcon = document.createElement('span')
       const leaderboardName = document.createElement('p')
       const leaderboardValue = document.createElement('p')
+
+      leaderboard.position = positionNumber
+      positionNumber++
+
+      if (leaderboard.position === 1)
+        leaderboardIcon.style.backgroundImage = `url("${leaderboard1}")`
+      else if (leaderboard.position === 2)
+        leaderboardIcon.style.backgroundImage = `url("${leaderboard2}")`
+      else if (leaderboard.position === 3)
+        leaderboardIcon.style.backgroundImage = `url("${leaderboard3}")`
+      else {
+        leaderboardIcon.className = 'position'
+        leaderboardIcon.textContent = leaderboard.position
+      }
 
       leaderboardName.textContent = leaderboard.user_id.username
 
@@ -64,7 +86,8 @@ class Stats {
           break
       }
 
-      leaderboardItem.append(leaderboardName, leaderboardValue)
+      leaderboardItem.append(leaderboardElement)
+      leaderboardElement.append(leaderboardIcon, leaderboardName, leaderboardValue)
     })
 
     this.domLeaderboard.append(leaderboardItem)
