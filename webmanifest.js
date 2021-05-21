b33a65d82f3aaab2fb13d9c101f6064c90c910ef
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-22 01:41:39 +0200

    manifest icons updated (try to fit with android)

--- webmanifest.js
+++ webmanifest.js
@@ -14,6 +14,84 @@ module.exports = {
   description: 'The new driving experience.',
   icons: [
     {
+      src: path.resolve('public/images/icon-16x16.png'),
+      size: '16x16',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-32x32.png'),
+      size: '32x32',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-57x57.png'),
+      size: '57x57',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-60x60.png'),
+      size: '60x60',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-72x72.png'),
+      size: '72x72',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-76x76.png'),
+      size: '76x76',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-96x96.png'),
+      size: '96x96',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-120x120.png'),
+      size: '120x120',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-128x128.png'),
+      size: '128x128',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-144x144.png'),
+      size: '144x144',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-152x152.png'),
+      size: '152x152',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-167x167.png'),
+      size: '167x167',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
+      src: path.resolve('public/images/icon-180x180.png'),
+      size: '180x180',
+      type: 'image/png',
+      purpose: 'any'
+    },
+    {
       src: path.resolve('public/images/icon-192x192.png'),
       size: '192x192',
       type: 'image/png',
