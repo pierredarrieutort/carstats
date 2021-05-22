6467ba0a8a8d0d042b676c3e83b5e3ed2ccf4ad1
Mathieu Daix <mathieudaixpro@gmail.com>
2021-05-22 14:26:36 +0200

    add copy plugin for correctly load images

--- webmanifest.js
+++ webmanifest.js
@@ -122,12 +122,6 @@ module.exports = {
       purpose: 'maskable'
     },
     {
-      src: path.resolve('public/images/ios-icon-512x512.png'),
-      size: '512x512',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
       src: path.resolve('public/images/maskable_icon_x384.png'),
       size: '384x384',
       type: 'image/png',
