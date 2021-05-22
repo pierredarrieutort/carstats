41f8801b7205c3de7324cd0248fa5630736d618e
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-22 12:55:45 +0200

    webmanifest optimization

--- webmanifest.js
+++ webmanifest.js
@@ -14,148 +14,18 @@ module.exports = {
   description: 'The new driving experience.',
   icons: [
     {
-      src: path.resolve('public/images/icon-16x16.png'),
-      size: '16x16',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-32x32.png'),
-      size: '32x32',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-57x57.png'),
-      size: '57x57',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-60x60.png'),
-      size: '60x60',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-72x72.png'),
-      size: '72x72',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-76x76.png'),
-      size: '76x76',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-96x96.png'),
-      size: '96x96',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-120x120.png'),
-      size: '120x120',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-128x128.png'),
-      size: '128x128',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-144x144.png'),
-      size: '144x144',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-152x152.png'),
-      size: '152x152',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-167x167.png'),
-      size: '167x167',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-180x180.png'),
-      size: '180x180',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-192x192.png'),
-      size: '192x192',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-256x256.png'),
-      size: '256x256',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
-      src: path.resolve('public/images/icon-384x384.png'),
-      size: '384x384',
-      type: 'image/png',
-      purpose: 'any'
-    },
-    {
       src: path.resolve('public/images/icon-512x512.png'),
-      size: '512x512',
+      sizes: [96, 128, 192, 256, 384, 512],
       type: 'image/png',
-      purpose: 'any'
+      purpose: 'any',
+      destination: path.join('assets/pwa-icons', 'any')
     },
     {
       src: path.resolve('public/images/maskable_icon_x512.png'),
-      size: '512x512',
+      sizes: [96, 128, 192, 256, 384, 512],
       type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x384.png'),
-      size: '384x384',
-      type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x192.png'),
-      size: '192x192',
-      type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x128.png'),
-      size: '128x128',
-      type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x96.png'),
-      size: '96x96',
-      type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x72.png'),
-      size: '72x72',
-      type: 'image/png',
-      purpose: 'maskable'
-    },
-    {
-      src: path.resolve('public/images/maskable_icon_x48.png'),
-      size: '48x48',
-      type: 'image/png',
-      purpose: 'maskable'
+      purpose: 'maskable',
+      destination: path.join('assets/pwa-icons', 'maskable')
     }
   ]
 }
