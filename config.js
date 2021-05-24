b350fa82aa2c16b2a64beb21c40bd3763f254340
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-24 21:03:44 +0200

    Temporary back URL

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default {
-  DOMAIN_URL: 'https://carstats-backend.herokuapp.com',
+  // DOMAIN_URL: 'https://carstats-backend.herokuapp.com',
+  DOMAIN_URL: 'https://carstats-back.herokuapp.com', // Temporary back URL
   MAPBOXGL: {
     ACCESS_TOKEN: 'pk.eyJ1IjoibWF0aGlldWRhaXgiLCJhIjoiY2tiOWI5ODgzMGNmYTJ6cGlnOTh5bjI5ZCJ9.061wCTnhLhD99yEEmz5Osw',
     STYLE: 'mapbox://styles/mathieudaix/ckkie2bdw0saz17pbidyjsgb4?optimize=true'
