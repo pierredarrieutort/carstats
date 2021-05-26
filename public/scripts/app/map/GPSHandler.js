a86d384a1cc218de33148192dea1690835114bc1
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-27 01:37:41 +0200

    add time divider

--- public/scripts/app/map/GPSHandler.js
+++ public/scripts/app/map/GPSHandler.js
@@ -110,7 +110,7 @@ export default class GPSHandler {
           const nextBearing = latestBearing + counter
 
           if (counter < loops) {
-            counter++
+            counter += loops / 100
             console.log(loops, counter)
             that.map.setBearing(nextBearing)
             window.requestAnimationFrame(bearingEase)
