1bb19e0fba31a3636341fdfda01d0a8114656733
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2021-05-29 13:02:03 +0200

    remove cron task

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import AuthApi from './public/scripts/utils/Api'
 import manifest from './dist/manifest.json'
 import favicon from 'serve-favicon'
 
-import cron from 'node-cron'
+// import cron from 'node-cron'
 
 const app = express()
 
@@ -109,6 +109,6 @@ app.get('/sw.js', (request, response) => {
 app.get('/manifest.webmanifest', (req, res) => res.json(manifest))
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')))
 
-cron.schedule('*/2 * * * *', function () {
-  console.log('running a task every two minutes keeping ON Heroku app.')
-})
+// cron.schedule('*/2 * * * *', function () {
+//   console.log('running a task every two minutes keeping ON Heroku app.')
+// })
