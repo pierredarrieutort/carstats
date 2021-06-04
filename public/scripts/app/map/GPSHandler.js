6627447a38b40238e601180b325211a0dac099c2
Mathieu Daix <mathieudaixpro@gmail.com>
2021-06-04 09:31:39 +0200

    fix easeTo

--- public/scripts/app/map/GPSHandler.js
+++ public/scripts/app/map/GPSHandler.js
@@ -118,7 +118,7 @@ export default class GPSHandler {
           duration: 200
         })
 
-        // window.requestAnimationFrame(bearingEase)
+        window.requestAnimationFrame(bearingEase)
       }
     }
 
