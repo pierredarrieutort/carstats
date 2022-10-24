9efcb042e8dda50cb0e6f4c3530890ebcfa03888
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2022-10-24 17:12:02 +0200

    prepare pass under Vue3

---
+++ carstats-vue-3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <br>
+    <router-link to="/app/navigation">Navigation</router-link> |
+    <router-link to="/app/statistics">Statistics</router-link> |
+    <router-link to="/app/driving">Driving</router-link> |
+    <router-link to="/app/profile">Profile</router-link>
+    <br>
+    <router-link to="/auth/forgot-password">Forgot Password</router-link> |
+    <router-link to="/auth/reset-password">Reset Password</router-link> |
+    <router-link to="/auth/sign-in">Sign in</router-link> |
+    <router-link to="/auth/sign-up">Sign up</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+@import '@/styles/main.scss';
+</style>
---
+++ carstats-vue-3/src/router/index.ts
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/app/HomeView.vue')
+  },
+  {
+    path: '/app/navigation',
+    name: 'navigation',
+    component: () => import('../views/app/NavigationView.vue')
+  },
+  {
+    path: '/app/statistics',
+    name: 'statistics',
+    component: () => import('../views/app/StatisticsView.vue')
+  },
+  {
+    path: '/app/driving',
+    name: 'driving',
+    component: () => import('../views/app/DrivingView.vue')
+  },
+  {
+    path: '/app/profile',
+    name: 'profile',
+    component: () => import('../views/app/ProfileView.vue')
+  },
+  {
+    path: '/auth/forgot-password',
+    name: 'Forgot Password',
+    component: () => import('../views/auth/ForgotPasswordView.vue')
+  },
+  {
+    path: '/auth/reset-password',
+    name: 'Reset Password',
+    component: () => import('../views/auth/ResetPasswordView.vue')
+  },
+  {
+    path: '/auth/sign-in',
+    name: 'Sign in',
+    component: () => import('../views/auth/SignInView.vue')
+  },
+  {
+    path: '/auth/sign-up',
+    name: 'Sign up',
+    component: () => import('../views/auth/SignUpView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
